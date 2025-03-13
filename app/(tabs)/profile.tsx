90219f7f26a7deed2e609618f40d5c928a2545c8
Md. Jamal Uddin <jaamaalxyz@gmail.com>
2025-03-13 09:26:49 +0600

    Create a simple form with basic components: ScrollView, Text, TextInput, Button, Alert, and StyleSheet

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,60 @@
-import { Text, View } from 'react-native';
+import React from 'react';
+import {
+  Alert,
+  Button,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+} from 'react-native';
 
 export default function Profile() {
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+
+  const handleSubmit = () => {
+    Alert.alert('Welcome', `${firstName} ${lastName}`);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Text>Profile</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text>{`My first name: ${firstName}`}</Text>
+      <TextInput
+        value={firstName}
+        onChangeText={setFirstName}
+        placeholder="Enter first name"
+        placeholderTextColor={'gray'}
+        style={styles.input}
+      />
+      <Text>{`My last name: ${lastName}`}</Text>
+      <TextInput
+        value={lastName}
+        onChangeText={setLastName}
+        placeholder="Enter last name"
+        placeholderTextColor={'gray'}
+        style={styles.input}
+      />
+      <Button
+        title="Submit"
+        onPress={handleSubmit}
+        disabled={!firstName || !lastName}
+      />
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
+  },
+  input: {
+    backgroundColor: 'lightgray',
+    padding: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    marginVertical: 10,
+  },
+});
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from 'expo-router';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1, backgroundColor: '#000' }}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
 }
